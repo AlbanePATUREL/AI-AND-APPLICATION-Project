a56fb79dee3d73f040881f8a23930ecc15d61416
AlbanePATUREL <150240551+AlbanePATUREL@users.noreply.github.com>
2025-11-12 08:35:48 +0100

    Add files via upload
    adding un fichier avec els commandes conda et un fichier qui donne selon une variable le taux de survie et aussi un graphique de survie selon l'âge

---
+++ testing.py
@@ -0,0 +1,31 @@
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+
+df=pd.read_csv("vesuvius_survival_dataset.csv")
+
+print(df.head())
+
+var=['DistanceFromV','WealthIndex','ShelterAccess','Status']
+for i in var:
+    print('Survival depending on :', i)
+    df2=df.groupby(i)['Survived'].mean().reset_index()
+    print(df2)
+    print('-'*10, '\n')
+
+fig = go.Figure()
+
+fig.add_trace(go.Histogram(x=df[df['Survived']==0]['Age'], name='Not Survived', opacity=0.5))
+fig.add_trace(go.Histogram(x=df[df['Survived']==1]['Age'], name='Survived', opacity=0.5))
+
+fig.update_layout(
+    title='Age Distribution by Survival',
+    xaxis_title='Age',
+    yaxis_title='Density',
+    barmode='overlay',  
+    bargap=0.1, 
+)
+
+fig.show()
